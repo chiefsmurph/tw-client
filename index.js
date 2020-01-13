b6a8a3d9b7c4a1645b4c643d5fb75d6b57ac4a89
chiefsmurph <chiefsmurph@gmail.com>
2020-01-12 20:44:16 -0800

    more

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 require('./utils/my-js');
 const initTW = require('./app-actions/init-tw');
 
+const { endPoint } = require('./config');
+
 const io = require('socket.io-client');
-const socket = io('http://chiefsmurph.com:3000');
+const socket = io(endPoint);
 const initSells = require('./app-actions/init-sells');
 const socketHandlers = require('./app-actions/socket-handlers');
 
